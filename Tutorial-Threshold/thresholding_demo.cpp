f91e5906d800cf5cc017156fe3dbe53a78ff59ee
ykkimhgu <38373000+ykkimhgu@users.noreply.github.com>
2021-02-17 15:14:53 +0900

    Create thresholding_demo.cpp

---
+++ Tutorial-Threshold/thresholding_demo.cpp
@@ -0,0 +1,44 @@
+#include <opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+//* @function main
+int main()
+{
+	Mat src, src_gray, dst, dst_morph;
+
+	src = imread("pcb_gray.jpg", 1);    // Load an image
+
+	if (src.empty())					// Load image check
+	{
+		cout << "File Read Failed : src is empty" << endl;
+		waitKey(0);
+	}
+	// Create a window to display results
+	namedWindow("DemoWIndow", CV_WINDOW_AUTOSIZE); //CV_WINDOW_AUTOSIZE(1) :Fixed Window, 0: Unfixed window
+	if (!src.empty())imshow("DemoWIndow", src); // Show image
+
+
+	/* threshold_type
+	0: Binary
+	1: Binary Inverted
+	2: Threshold Truncated
+	3: Threshold to Zero
+	4: Threshold to Zero Inverted*/
+	int threshold_value = 130;
+	int threshold_type = 0;
+	int const max_value = 255;
+	int const max_type = 4;
+	int const max_BINARY_value = 255;
+
+	threshold(src, dst, threshold_value, max_BINARY_value, threshold_type);
+
+	// Create a window to display results
+	namedWindow("ThreshWIndow", CV_WINDOW_AUTOSIZE); //CV_WINDOW_AUTOSIZE(1) :Fixed Window, 0: Unfixed window
+	imshow("ThreshWIndow", dst); // Show image
+		
+	waitKey(0);//Pause the program
+	return 0;
+}

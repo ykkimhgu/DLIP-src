7a7610549bb4ed5f7da2dfc1218f19b2ff3f327d
youjh005 <youjh@handong.edu>
2021-02-22 21:50:19 +0900

    Add files via upload

---
+++ Tutorial-OpenCV/03_basic_image_operation.cpp
@@ -0,0 +1,36 @@
+///*------------------------------------------------------/
+//* Image Proccessing with Deep Learning
+//* OpenCV Tutorial: Basic Iamge Operation (Crop, Rotate, Resize)
+//* Created: 2021-Spring
+//------------------------------------------------------*/
+//#include <iostream>
+//#include <opencv2/opencv.hpp>
+//
+//using namespace std;
+//using namespace cv;
+//
+//int main()
+//{
+//	/*  read image  */
+//	Mat img = imread("image.jpg");
+//	imshow("img", img);
+//
+//	/*  Crop(Region of Interest)  */
+//	Rect r(10, 10, 150, 150);	 // (x, y, width, height)
+//	Mat roiImg = img(r);
+//	imshow("roiImg", roiImg);
+//
+//	/*  Rotate  */
+//	Mat rotImg;
+//	rotate(img, rotImg, ROTATE_90_CLOCKWISE);
+//	imshow("rotImg", rotImg);
+//
+//	/*  Resize  */
+//	Mat resizedImg;
+//	resize(img, resizedImg, Size(1000, 100));
+//	imshow("resizedImg", resizedImg);
+//
+//	waitKey(0);
+//}
+//
+//

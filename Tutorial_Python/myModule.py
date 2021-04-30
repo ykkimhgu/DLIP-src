4848283bbbf4e6312bfbd09c0c4385cc8dc2c315
JiHwan You <77210239+deus19950808@users.noreply.github.com>
2021-04-30 14:03:26 +0900

    Create myModule.py

---
+++ Tutorial_Python/myModule.py
@@ -0,0 +1,19 @@
+# mymodule.py
+ 
+  class Person:
+    def __init__(self, fname, lname):
+      self.firstname = fname
+      self.lastname = lname
+ 
+    def printname(self):
+      print(self.firstname, self.lastname)
+ 
+ 
+    person1 = {
+      "name": "John",
+      "age": 36,
+      "country": "Norway"
+    }
+ 
+    def greeting(name):
+      print("Hello, " + name)

f82a7a463ee91c59c7c1f61ad0f9248a940654e8
Jan03 <jep1009@hanmail.net>
2022-03-08 12:38:35 +0900

    Update shallow_deep_copy.cpp

--- Tutorial_OpenCV/shallow_deep_copy.cpp
+++ Tutorial_OpenCV/shallow_deep_copy.cpp
@@ -5,27 +5,28 @@
 ------------------------------------------------------*/
 #include <iostream>
 #include <opencv2/opencv.hpp>
-
+ 
 using namespace std;
 using namespace cv;
-
+ 
 int main()
 {
-
+ 
 	Mat src, dst_shallow, dst_deep;
 	// read image  
-	src = imread("image.jpg", 0);   //graysacle
-
+	src = imread("image.jpg", 0);
+	
 	/* Shallow Copy */
 	dst_shallow = src;
-	flip(dst_shallow, dst_shallow, 1);
-	imshow("src after shallow copy", src);
-
+ 
 	/* Deep Copy */
 	src.copyTo(dst_deep);
-	flip(dst_deep, dst_deep, 1);
-	imshow("src after deep copy", src);
-
+ 
+	flip(src, src, 1);
+ 
+	imshow("dst_shallow", dst_shallow);
+	imshow("dst_deep", dst_deep);
 	waitKey(0);
 	return 0;
 }
+

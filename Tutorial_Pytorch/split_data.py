75ad18b443962cdf491dec1fdf2cf895fbd05c5d
ykkimhgu <38373000+ykkimhgu@users.noreply.github.com>
2022-05-20 16:21:44 +0900

    Add files via upload

---
+++ Tutorial_Pytorch/split_data.py
@@ -0,0 +1,69 @@
+import os, shutil, random
+
+# preparing the folder structure
+
+full_data_path = 'dataset_mask/archive/obj/'
+extension_allowed = '.jpg'
+split_percentage = 90
+
+images_path = 'dataset_mask/images/'
+if os.path.exists(images_path):
+    shutil.rmtree(images_path)
+os.mkdir(images_path)
+    
+labels_path = 'dataset_mask/labels/'
+if os.path.exists(labels_path):
+    shutil.rmtree(labels_path)
+os.mkdir(labels_path)
+    
+training_images_path = images_path + 'training/'
+validation_images_path = images_path + 'validation/'
+training_labels_path = labels_path + 'training/'
+validation_labels_path = labels_path +'validation/'
+    
+os.mkdir(training_images_path)
+os.mkdir(validation_images_path)
+os.mkdir(training_labels_path)
+os.mkdir(validation_labels_path)
+
+files = []
+
+ext_len = len(extension_allowed)
+
+for r, d, f in os.walk(full_data_path):
+    for file in f:
+        if file.endswith(extension_allowed):
+            strip = file[0:len(file) - ext_len]      
+            files.append(strip)
+
+random.shuffle(files)
+
+size = len(files)                   
+
+split = int(split_percentage * size / 100)
+
+print("copying training data")
+for i in range(split):
+    strip = files[i]
+                         
+    image_file = strip + extension_allowed
+    src_image = full_data_path + image_file
+    shutil.copy(src_image, training_images_path) 
+                         
+    annotation_file = strip + '.txt'
+    src_label = full_data_path + annotation_file
+    shutil.copy(src_label, training_labels_path) 
+
+print("copying validation data")
+for i in range(split, size):
+    strip = files[i]
+                         
+    image_file = strip + extension_allowed
+    src_image = full_data_path + image_file
+    shutil.copy(src_image, validation_images_path) 
+                         
+    annotation_file = strip + '.txt'
+    src_label = full_data_path + annotation_file
+    shutil.copy(src_label, validation_labels_path) 
+
+print("finished")
---
+++ Tutorial_Pytorch/visualizeLabel.py
@@ -0,0 +1,32 @@
+import cv2
+
+image_path = 'dataset_mask/archive/obj/2-with-mask'
+
+image = cv2.imread(image_path + '.jpg')
+
+class_list = ['using mask', 'without mask']
+colors = [(0, 255, 0), (0, 255, 255)]
+
+height, width, _ = image.shape
+
+T=[]
+with open(image_path + '.txt', "r") as file1:
+    for line in file1.readlines():
+        split = line.split(" ")
+
+        # getting the class id
+        class_id = int(split[0])
+        color = colors[class_id]
+        clazz = class_list[class_id]
+
+        # getting the xywh bounding box coordinates
+        x, y, w, h = float(split[1]), float(split[2]), float(split[3]), float(split[4])
+
+        # re-scaling xywh to the image size
+        box = [int((x - 0.5*w)* width), int((y - 0.5*h) * height), int(w*width), int(h*height)]
+        cv2.rectangle(image, box, color, 2)
+        cv2.rectangle(image, (box[0], box[1] - 20), (box[0] + box[2], box[1]), color, -1)
+        cv2.putText(image, class_list[class_id], (box[0], box[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, .5, (0,0,0))
+
+cv2.imshow("output", image)
+cv2.waitKey()

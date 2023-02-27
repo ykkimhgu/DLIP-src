403fb735c9dadb909b6a84f613c179d88a7c5471
ykkimhgu <38373000+ykkimhgu@users.noreply.github.com>
2023-02-27 14:35:14 +0900

    Add files via upload

---
+++ Tutorial_Cpp/DLIP_Tutorial_C++_define_function_tutorial.cpp
@@ -0,0 +1,20 @@
+/*------------------------------------------------------/
+* Image Proccessing with Deep Learning
+* Cpp Tutorial: Define function
+* Created: 2022-Spring
+------------------------------------------------------*/
+
+#include <iostream>
+
+int main() {
+
+	int val1 = 11;
+	int val2 = 22;
+
+	int out = val1 + val2;
+
+	std::cout << out << std::endl;
+
+	system("pause");
+	return 0;
+}

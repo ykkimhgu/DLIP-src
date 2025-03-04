014486f7b0aa08fdfca8a55aa86ce691c43e7f96
YangJiWoo7240 <yangjiu2504@gmail.com>
2025-03-04 11:02:38 +0900

    Update and rename Assignment_Eigenvalue_ID.cpp to DLIP_Assign_assignName_ID.cpp

--- Assignment/DLIP_Assign_assignName_ID.cpp
+++ Assignment/DLIP_Assign_assignName_ID.cpp
@@ -1,13 +1,13 @@
 /*------------------------------------------------------------------------------------------\
-@ Numerical Programming  by Young-Keun Kim - Handong Global University
+@ Deep Learning & Image Processing  by Young-Keun Kim - Handong Global University
 
 Author          : Young-Keun Kim
 Created         : 01-04-2019
 Modified        : 01-04-2023
-Language/ver	: C in MSVS2017
-Course			: Numerical Programming 
+Language/ver	: C++
+Course			: Deep Learning & Image Processing
 
-Description     : Assignment 7 Eigenvalue,Eigenvector
+Description     : Assignment 7 Eigenvalue,Eigenvector (ex)
 /------------------------------------------------------------------------------------------*/
 
 
